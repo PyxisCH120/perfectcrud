f94c2f3ca668cb78788adbb479c6b5e97f79e5db
mateus <mateusmicaelpeixoto@gmail.com>
2024-06-08 00:02:10 -0300

    ADS:01609372 DB

--- pages/api/app.js
+++ pages/api/app.js
@@ -1,29 +1,34 @@
 /**
- * Nome do arquivo: ordens.js
- * Data de criação:24/06/2024
- * Autor: mateus micael p.rabelo
+ * Nome do arquivo: exemplo.js
+ * Data de criação: 24/04/2024
+ * Autor: mateus micael peixoto
  * Matrícula: 01609372
  *
  * Descrição:
- * Este arquivo é responsável pelo CRUD da collection ordens de serviço
- * fazendo a manipulação dos dados conformme o tipo de requisição realizada.
+ * Este código inicializa o Firebase em um aplicativo React 
+ * usando useEffect. Ele verifica se o Firebase já foi inicializado
+ *  e, se não, o faz com a configuração fornecida. O componente MyApp 
+ * retorna o Component com as props pageProps e é exportado como o componente
+ *  padrão do módulo.
  *
- * Este script é parte o curso de ADS.
+ * Este script é parte o curso de ADS.
  */
 
 import { useEffect } from "react";
 import firebase from "firebase/app";
-import "firebase/firestore";
+import "firebase/firestore"; 
 
-// import firebaseConfig from "../config/firebaseconfig";
-import firebaseConfig from "../../config/firebaseconfig";
+import firebaseConfig from "../config/firebaseconfig"; 
+
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
 function MyApp({ Component, pageProps }) {
+  
   useEffect(() => {
+    
     if (!firebase.apps.length) {
       firebase.initializeApp(firebaseConfig);
     }
--- pages/api/users.js
+++ pages/api/users.js
@@ -1,26 +1,59 @@
 /**
- * Nome do arquivo: users.js
- * Data de criação:24/06/2024
- * Autor: mateus micael p.rabelo
+ * Nome do arquivo: exemplo.js
+ * Data de criação: 24/04/2024
+ * Autor: mateus micael peixoto
  * Matrícula: 01609372
  *
  * Descrição:
- * Este arquivo é responsável pelo CRUD da collection de clients
- * fazendo a manipulação dos dados conformme o tipo de requisição realizada.
+ *  implementa um servidor HTTP usando o Next.js framework.
+ *  Quando recebe uma requisição, verifica o método HTTP. Se for
+ *  GET, retorna uma lista de pessoas. Se for POST, adiciona uma nova pessoa 
+ * à lista. Se for PUT, atualiza os dados de uma pessoa existente. Se for DELETE, remove uma
+ *  pessoa da lista. Se a requisição não corresponder a nenhum desses métodos, retorna um código de
+ *  status 405 (Método não permitido).
  *
- * Este script é parte o curso de ADS.
+ * Este script é parte o curso de ADS.
  */
 
-export default function handler(req, res) {
-  if (req.method == "GET") {
-    res.status(200).json("Local onde será retornado os dados dos clientes");
-  } else if (req.method == "POST") {
-    res.status(200).json("Local onde serão adicionadas novos clientes");
-  }
 
-  if (req.method == "PUT") {
-    res.status(200).json("Local onde os clientes terão seus dados alterados");
-  } else if (req.method == "DELETE") {
-    res.status(200).json("Local onde os clientes serão apagados");
+
+let people = [
+  { id: 1, name: "João Silva", email: "joao@example.com" },
+  { id: 2, name: "Maria Souza", email: "maria@example.com" },
+];
+
+export default function handler(req, res) {
+  if (req.method === "GET") {
+    res.status(200).json(people);
+  } else if (req.method === "POST") {
+    const { name, email } = req.body;
+    const newPerson = { id: people.length + 1, name, email };
+    people.push(newPerson);
+    res.status(201).json(newPerson);
+  } else if (req.method === "PUT") {
+    const { id } = req.query;
+    const { name, email } = req.body;
+    const personIndex = people.findIndex(
+      (person) => person.id === parseInt(id)
+    );
+    if (personIndex !== -1) {
+      people[personIndex] = { id: parseInt(id), name, email };
+      res.status(200).json(people[personIndex]);
+    } else {
+      res.status(404).json({ message: "Pessoa não encontrada" });
+    }
+  } else if (req.method === "DELETE") {
+    const { id } = req.query;
+    const personIndex = people.findIndex(
+      (person) => person.id === parseInt(id)
+    );
+    if (personIndex !== -1) {
+      people.splice(personIndex, 1);
+      res.status(200).json({ message: "Pessoa excluída com sucesso" });
+    } else {
+      res.status(404).json({ message: "Pessoa não encontrada" });
+    }
+  } else {
+    res.status(405).json({ message: "Método não permitido" });
   }
 }
